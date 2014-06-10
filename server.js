9f9b7ef3a9d9631b27a6ac05db3cb607dddb7bfa
rayito <rayito@MacBook-Pro-de-Eden.local>
2014-06-10 23:32:43 +0200

    Version 1.0 to deploy

--- server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var http = require('http');
- 
-var app = express();
-app.use(express.static(__dirname + '/static'));
- 
-http.createServer(app).listen(8081);
-console.log("server is listening on 8081. Access http://localhost:8081/index.html");
