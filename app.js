6f991b6f9efea7b1042981d3ffcd5a3f855962aa
rayito <rayito@MacBook-Pro-de-Eden.local>
2014-06-11 00:35:32 +0200

    To deploy

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(enableCORS);
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
+  //app.set('views', __dirname + '/views');
   
   // app.set('view engine', 'jade');
   app.use(express.favicon());
